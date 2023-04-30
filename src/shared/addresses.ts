35aa20a4ad57b22c5a57834b207bb3581edb0335
Miyamura80 <eitomiyamura@gmail.com>
2023-04-30 15:56:59 +0100

    got zk mvp working

---
+++ src/shared/addresses.ts
@@ -0,0 +1,8 @@
+// file: /src/shared/addresses.ts
+
+export const Addresses = {
+    SIMPLE_MULTIPLIER_ADDR: "0x213a6e0147061eeb95d36c7c5ee929245d1bf0cb" as `0xsimpleMultiplier`,
+  }
+
+  // Plonk: 0x226266054081624f7f1f8405bbe21b8d0b349fba
+  // Simple Multiplier: 0x213a6e0147061eeb95d36c7c5ee929245d1bf0cb
